dccd165b1588395dde3b78e6536f87bbce1c61ee
Sasha Spala <sashaspala@Sashas-MacBook-Air.local>
2025-01-21 13:06:12 -0500

    setting up for deployment from gitlab pages

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-    paths: function (paths, env) {
-        // Changing public to static
-        paths.appPublic = path.resolve(__dirname, 'public_html');
-        paths.appHtml = path.resolve(__dirname, 'public_html/index.html');
-        return paths;
-    }
-}
